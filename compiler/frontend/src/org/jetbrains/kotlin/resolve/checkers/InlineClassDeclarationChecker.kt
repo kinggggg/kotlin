29ae3198a807c7ea6251c77fd4d92fdf77b334b7
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-05-06 03:31:16 +0300

    Require presence of public primary constructor for inline class

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/checkers/InlineClassDeclarationChecker.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/checkers/InlineClassDeclarationChecker.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.lexer.KtModifierKeywordToken
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtDeclaration
@@ -17,6 +18,7 @@ import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.getModificationStamp
 import org.jetbrains.kotlin.psi.psiUtil.hasActualModifier
 import org.jetbrains.kotlin.psi.psiUtil.modalityModifier
+import org.jetbrains.kotlin.psi.psiUtil.visibilityModifier
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 
 object InlineClassDeclarationChecker : DeclarationChecker {
@@ -44,6 +46,15 @@ object InlineClassDeclarationChecker : DeclarationChecker {
         if (primaryConstructor == null) {
             trace.report(Errors.ABSENCE_OF_PRIMARY_CONSTRUCTOR_FOR_INLINE_CLASS.on(inlineKeyword))
             return
+        }
+
+        val primaryConstructorVisibilityModifier = primaryConstructor.visibilityModifier()
+        if (primaryConstructorVisibilityModifier != null) {
+            val elementType = primaryConstructorVisibilityModifier.node.elementType
+            if (elementType != KtTokens.PUBLIC_KEYWORD) {
+                trace.report(Errors.NON_PUBLIC_PRIMARY_CONSTRUCTOR_OF_INLINE_CLASS.on(primaryConstructorVisibilityModifier))
+                return
+            }
         }
 
         val baseParameter = primaryConstructor.valueParameters.singleOrNull()
